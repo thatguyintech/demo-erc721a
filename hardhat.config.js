d9606383bcedf7b12af4c88f044ff37fc5f94f32
ALBERT <thatguyintech@gmail.com>
2022-01-25 00:53:00 -0800

    setup hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require('dotenv').config();
+
 module.exports = {
-  solidity: "0.7.3",
-};
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  solidity: "0.8.6",
+  paths: {
+    sources: "./src",
+    tests: "./test",
+    artifacts: "./artifacts"
+  },
+}
